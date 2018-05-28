9010c672f62bc457ff3676299f353ad5edd6c79d
Duy Dương <phanduyduong97@gmail.com>
2018-05-28 16:54:12 +0700

    [Duy DƯơng] thêm dữ liệu bảng trình đọ học vấn

---
+++ Insert data.sql
@@ -0,0 +1,22 @@
+﻿USE QuanLyNhanSu
+GO
+INSERT dbo.TrinhDoHocVan
+VALUES  ( 'TD01',N'Đại học',N'Công nghệ thông tin' )
+INSERT dbo.TrinhDoHocVan
+VALUES  ( 'TD02',N'Cao học',N'Cơ khí' )
+INSERT dbo.TrinhDoHocVan
+VALUES  ( 'TD03',N'Cao đẳng',N'Kế toán' )
+INSERT dbo.TrinhDoHocVan
+VALUES  ( 'TD04',N'Trung cấp',N'Công nghệ thông tin' )
+INSERT dbo.TrinhDoHocVan
+VALUES  ( 'TD05',N'Kỹ sư',N'Cầu đường' )
+INSERT dbo.TrinhDoHocVan
+VALUES  ( 'TD06',N'Thạc sỹ',N'Công nghệ thông tin' )
+INSERT dbo.TrinhDoHocVan
+VALUES  ( 'TD07',N'Tiến sỹ',N'Cơ khí' )
+INSERT dbo.TrinhDoHocVan
+VALUES  ( 'TD08',N'Giáo sư',N'Công nghệ thông tin' )
+INSERT dbo.TrinhDoHocVan
+VALUES  ( 'TD09',N'Phó giáo sư',N'Hóa phân tích' )
+INSERT dbo.TrinhDoHocVan
+VALUES  ( 'TD10',N'Phó tiến sỹ',N'Công nghệ thông tin' )
